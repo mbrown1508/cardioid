83ea273feee095fc12e82424078abfc3f8a92146
Matt <mbrown1508@outlook.com>
2019-02-21 00:25:31 +1000

    never ending cardioid

---
+++ cardioid.py
@@ -0,0 +1,79 @@
+import pygame
+import math
+from random import randint
+
+WIDTH, HEIGHT = 800, 800
+X_OFFSET, Y_OFFSET = WIDTH / 2, HEIGHT / 2
+
+pygame.init()
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Cardioid")
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  11, 102,   35)
+RED =   (255,   0,   0)
+OFF_WHITE = (225, 225, 225)
+
+
+def circle(pos, radius, color=WHITE):
+    pygame.draw.circle(win, color, (int(pos[0]+X_OFFSET), int(pos[1]+Y_OFFSET)), int(radius), 1)
+
+
+def line(pos1, pos2, color=WHITE):
+    pygame.draw.line(win, color, (int(pos1[0]+X_OFFSET), int(pos1[1]+Y_OFFSET)), (int(pos2[0]+X_OFFSET), int(pos2[1]+Y_OFFSET)), 1)
+
+
+total = 200
+step = math.pi * 2 / total
+factor = 6000
+offset = ()
+r = WIDTH / 2.2
+
+
+def get_vector(index):
+    angle = (index * step) % total
+
+    x = r * math.cos(angle)
+    y = r * math.sin(angle)
+
+    return x, y
+
+
+run = True
+clock = pygame.time.Clock()
+while run:
+    dt = clock.tick(60)
+
+
+    factor += 0.01
+
+    win.fill(BLACK)
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+            # elif event.key == pygame.K_SPACE:
+            # #     pass
+            # # elif event.key == pygame.K_r:
+            # #     started = False
+        elif event.type == pygame.QUIT:
+            run = False
+
+    for i in range(total):
+        a = i
+        b = i * factor
+
+        a_pos = get_vector(a)
+        b_pos = get_vector(b)
+
+        line(a_pos, b_pos)
+
+    # Draw a circle
+    circle([0, 0], r)
+
+    pygame.display.update()
+
+pygame.quit()
